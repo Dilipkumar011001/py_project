2ff37137f8007fdbc5268f7586dba9c6dc42f600
Dilipkumar011001 <Dilipkumar.10747959@ltimindtree.com>
2025-08-13 15:57:56 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+ 
+app = Flask(__name__)
+ 
+@app.route("/")
+def home():
+    return "Calculator API Running!"
+ 
+@app.route("/add")
+def add():
+    try:
+        a = float(request.args.get("a", 0))
+        b = float(request.args.get("b", 0))
+        return jsonify(result=a + b)
+    except Exception as e:
+        return jsonify(error=str(e)), 400
+ 
+@app.route("/sub")
+def sub():
+    try:
+        a = float(request.args.get("a", 0))
+        b = float(request.args.get("b", 0))
+        return jsonify(result=a - b)
+    except Exception as e:
+        return jsonify(error=str(e)), 400
+ 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+ 
+ 
